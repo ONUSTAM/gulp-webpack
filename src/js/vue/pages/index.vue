1ce992ad243ceb40f2de62da77e3fb5839293a90
matsuno-t <matsuno-t@iij.ad.jp>
2016-08-16 09:28:05 +0900

    edit

---
+++ src/js/vue/pages/index.vue
@@ -0,0 +1,24 @@
+<template lang="html">
+  <div class="container-fulid">
+    <h3 class="text-center">index</h3>    
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Index',
+  data: function () {
+    return {
+    }
+  },
+  computed: {},
+  ready: function () {},
+  attached: function () {},
+  methods: {},
+  components: {}
+}
+</script>
+
+<style lang="css">
+</style>
